f3c31185aa90a921929b8c40ed3e1ce2a6531586
Joel Lacour <lacour.joel@web.de>
2023-09-20 13:47:29 +0200

    create webtransport echo server

--- server/go.mod
+++ server/go.mod
@@ -14,11 +14,15 @@ require (
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/onsi/ginkgo/v2 v2.9.5 // indirect
+	github.com/quic-go/qpack v0.4.0 // indirect
 	github.com/quic-go/qtls-go1-20 v0.3.3 // indirect
+	github.com/quic-go/webtransport-go v0.5.3 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
 	golang.org/x/exp v0.0.0-20221205204356-47842c84f3db // indirect
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/tools v0.9.1 // indirect
 )
--- server/src/main.go
+++ server/src/main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"encoding/binary"
+	"fmt"
 	"net"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
 	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/http3"
+	"github.com/quic-go/webtransport-go"
 )
 
 func main() {
@@ -27,12 +33,95 @@ func main() {
 	cwd, _ := os.Getwd()
 	log.Info().Msg("Current directory: " + cwd)
 
-	startServer()
+	startWebtransportServer()
 }
 
-func startServer() error {
+func startWebtransportServer() error {
 
-	udpConnection, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 8080})
+	router := http.NewServeMux()
+
+	certificate, err := tls.LoadX509KeyPair("certs/cert_dev.pem", "certs/key_dev.pem")
+	if err != nil {
+		log.Err(err).Msg("Failed to load TLS certificate")
+		return err
+	}
+
+	tlsConfiguration := tls.Config{Certificates: []tls.Certificate{certificate}}
+
+	server := webtransport.Server{
+		H3: http3.Server{
+			TLSConfig: &tlsConfiguration,
+			Addr:      "127.0.0.1:8080",
+			Handler:   router,
+		},
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+
+	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hallo!"))
+	})
+
+	router.HandleFunc("/connect", func(w http.ResponseWriter, r *http.Request) {
+
+		wtConnection, err := server.Upgrade(w, r)
+		if err != nil {
+			log.Err(err).Msg("Failed to upgrade")
+		}
+
+		wtStream, err := wtConnection.AcceptStream(context.Background())
+		if err != nil {
+			log.Err(err).Msg("Failed to open stream")
+		}
+		defer wtStream.Close()
+
+		for {
+			log.Debug().Msg("Waiting for message")
+			buffer := make([]byte, 1024)
+
+			readCount, err := wtStream.Read(buffer)
+
+			if err != nil {
+				log.Err(err).Msg("Failed to read data")
+			}
+
+			message := string(buffer[2:readCount])
+			log.Debug().Int("read-count", readCount).Str("data", message).Msg("Received data")
+
+			responseMessage := strings.ToUpper(message)
+			responseLength := len(responseMessage)
+			responseData := make([]byte, responseLength+2)
+
+			binary.BigEndian.PutUint16(responseData, uint16(responseLength))
+			copy(responseData[2:], []byte(responseMessage))
+
+			log.Debug().Msg(fmt.Sprintf("data: %v", responseData))
+
+			writeCount, err := wtStream.Write(responseData)
+			if err != nil {
+				log.Err(err).Msg("Failed to write response")
+			}
+
+			log.Debug().Int("write-count", writeCount).Msg("Wrote response message")
+
+		}
+	})
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Err(err).Msg("Failed to start listening")
+		return err
+	}
+
+	log.Info().Msg("Listening to connections")
+
+	return nil
+}
+
+func startQuicServer() error {
+
+	ipAddress := net.ParseIP("127.0.0.1")
+	udpAddress := net.UDPAddr{IP: ipAddress, Port: 8080}
+
+	udpConnection, err := net.ListenUDP("udp", &udpAddress)
 	if err != nil {
 		log.Err(err).Msg("UDP server could not start")
 		return err
@@ -61,6 +150,25 @@ func startServer() error {
 	}
 
 	log.Info().Msg(connection.RemoteAddr().String())
+
+	stream, err := connection.AcceptStream(context.Background())
+	if err != nil {
+		log.Err(err).Msg("Failed to accept stream")
+		return err
+	}
+	defer stream.Close()
+
+	log.Info().Msg("Accepted stream")
+
+	data := make([]byte, 100)
+	readCount, err := stream.Read(data)
+
+	if err != nil {
+		log.Err(err).Msg("Failed to read from stream")
+		return err
+	}
+
+	log.Info().Int("read count", readCount).Msg("Received data: " + string(data))
 
 	return nil
 }
