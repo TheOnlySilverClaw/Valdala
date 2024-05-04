66345926678b72f91310322382d1a60b2211daa1
Joel Lacour <lacour.joel@web.de>
2024-05-04 18:00:27 +0200

    use log levels

--- application/main.v
+++ application/main.v
@@ -1,45 +1,48 @@
 module main
 
+import log
+
 import glfw
 import webgpu
 
+
 fn main() {
 
-	println("launch")
+	log.info("launch")
 	
 	instance := webgpu.create_instance()!
 	defer {instance.release()}
-	println("created instance")
+	log.info("created instance")
 
 	window := glfw.open_window(1200, 1000, "YAMC")!
 	defer { glfw.terminate() }
 	defer { window.destroy() }
-	println("created window")
+	log.info("created window")
 
 	surface := window.get_surface(instance)
 	defer { surface.release() }
-	println("created surface")
+	log.info("created surface")
 
 	adapter := instance.request_adapter(surface) or {
-		println("failed to get adapter")
+		log.info("failed to get adapter")
 		return
 	}
 	defer { adapter.release() }
-	println("created adapter")
+	log.info("created adapter")
 
 	device := adapter.request_device() or {
-		println("failed to get device")
+		log.info("failed to get device")
 		return
 	}
 	defer { device.release() }
-	println("created device")
+	log.info("created device")
 
 	queue := device.get_queue()
 	defer { queue.release() }
-	println("created queue")
+	log.info("created queue")
 
 	surface.configure(adapter, device)
-	println("surface configured")
+	log.info("surface configured")
 
 	for !window.should_close() {
 
@@ -49,34 +52,34 @@ fn main() {
 		glfw.poll_events()
 	}
 
-	println("done")
+	log.info("done")
 }
 
 fn render(device webgpu.WGPUDevice, surface webgpu.WGPUSurface, queue webgpu.WGPUQueue) ! {
 
 	surface_texture := surface.get_current_texture()!
 	defer { surface_texture.release() }
-	println("got current surface texture")
+	log.debug("got current surface texture")
 
 	frame := surface_texture.get_view(1)
 	defer { frame.release() }
-	println(frame)
+	log.debug("got current frame texture")
 
 	command_encoder := device.create_command_encoder("encoder")
 	defer { command_encoder.release() }
-	println("command encoder created")
+	log.debug("command encoder created")
 
 	render_pass_encoder := command_encoder.begin_render_pass(frame)
 	defer { render_pass_encoder.release() }
 	render_pass_encoder.end()
-	println("pass encoder created")
+	log.debug("pass encoder created")
 
 	command_buffer := command_encoder.finish()
 	defer { command_buffer.release() }
-	println("command buffer created")
+	log.debug("command buffer created")
 
 	queue.submit(&command_buffer)
-	println("command submitted")
+	log.debug("command submitted")
 
 	surface.present()
 }
