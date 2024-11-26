1cef4be9e58e2bd7d07fa8e8ce3dfaf015333357
Joel <silverclaw@noreply.codeberg.org>
2024-11-26 18:56:03 +0100

    fix test root

--- application/build.zig
+++ application/build.zig
@@ -54,7 +54,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("source/tests.zig"),
+        .root_source_file = b.path("source/test.zig"),
         .target = target,
         .optimize = optimize,
     });
