d8d5ead197e9e9e3b0dc50ae50495617d73b66e5
Joel Lacour <silverclaw@noreply.codeberg.org>
2025-02-26 18:43:14 +0100

    parse and free arguments

--- application/source/main.zig
+++ application/source/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const log = std.log;
+const process = std.process;
 const Application = @import("Application.zig");
 
 pub fn main() void {
@@ -7,6 +8,13 @@ pub fn main() void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
 
     const allocator = gpa.allocator();
+
+    const args = process.argsAlloc(allocator) catch {
+        log.err("Failed to allocate arguments", .{});
+        return;
+    };
+
+    std.log.debug("args: {s}", .{ args });
 
     var application = Application.init(allocator) catch |err| {
         log.err("Failed to initialize: {}", .{ err });
@@ -18,6 +26,7 @@ pub fn main() void {
     };
     
     application.deinit();
+    process.argsFree(allocator, args);
     
     const check = gpa.deinit();
     if(check == .leak) {
