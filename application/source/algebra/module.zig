5abedc2a4d39716b45e3d5e21f1769d56c726cae
Brage <bragewiseth@icloud.com>
2025-03-23 18:32:17 +0100

    changed filename to match conventions

--- application/source/algebra/module.zig
+++ application/source/algebra/module.zig
@@ -2,4 +2,4 @@ pub const vector = @import("vector.zig");
 pub const matrix = @import("matrix.zig");
 pub const Quaternion = @import("quaternion.zig").Quaternion;
 pub const Transform = @import("transform.zig").Transform;
-pub const Axis = @import("Axis.zig").Axis;
+pub const Axis = @import("axis.zig").Axis;
