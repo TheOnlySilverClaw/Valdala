bd63c6149b577b1e4cc65c5c73619189fe2b3d3a
Joel Lacour <silverclaw@noreply.codeberg.org>
2025-04-29 18:23:32 +0200

    minor logger changes

--- src/log.zig
+++ src/log.zig
@@ -8,8 +8,6 @@ pub fn pretty(comptime message_level: std.log.Level, comptime scope: @TypeOf(.en
 }
 
 fn write(comptime message_level: std.log.Level, comptime scope: @TypeOf(.enum_literal), comptime format: []const u8, args: anytype) !void {
-    
-    var writer = std.io.getStdOut().writer();
 
     const color_code = switch (message_level) {
         .err => "\x1b[1;31m",
@@ -18,8 +16,6 @@ fn write(comptime message_level: std.log.Level, comptime scope: @TypeOf(.enum_li
         .debug => "\x1b[39m"
     };
 
-    try writer.writeAll(color_code);
-
     const level_string = switch (message_level) {
         .debug => "DEBUG",
         .info => "INFO",
@@ -27,7 +23,8 @@ fn write(comptime message_level: std.log.Level, comptime scope: @TypeOf(.enum_li
         .err => "ERROR"
     };
 
-    try std.fmt.format(writer, "{d}  {s:<5}  @{s:<12}  ", .{ std.time.milliTimestamp(), level_string, @tagName(scope) });
+    var writer = std.io.getStdOut().writer();
+    try std.fmt.format(writer, "{s} {d}  {s:<5}  @{s:<12}  ", .{ color_code, std.time.milliTimestamp(), level_string, @tagName(scope) });
     try std.fmt.format(writer, format, args);
     try writer.writeAll("\n\x1b[0m");
 }
