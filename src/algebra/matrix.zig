d463840e075b9ca0eef084a3344094ea53ed88ff
Joel Lacour <silverclaw@noreply.codeberg.org>
2025-04-30 22:42:39 +0200

    rewrite matrix type

---
+++ src/algebra/matrix.zig
@@ -0,0 +1,220 @@
+const std = @import("std");
+const math = std.math;
+
+pub fn Matrix(comptime T: type, comptime columns: u32, comptime rows: u32) type {
+
+    return struct {
+
+        const Self = @This();
+
+        pub const C = columns;
+        pub const R = rows;
+
+        const length = C * R;
+
+        values: [C * R]T,
+
+        pub fn of(values: [length] T) Self {
+            return .{ .values = values };
+        }
+        
+        pub fn all(value: T) Self {
+            return of(.{ value } ** length);
+        }
+
+        pub fn zero() Self {
+            return all(0);
+        }
+
+        pub fn identity() Self {
+
+            var m = zero();
+            m.setDiagonal(.{ 1 } ** C);
+            return m;
+        }
+
+        fn indexOf(column: u32, row: u32) usize {
+            return R * column + row;
+        }
+
+        pub fn get(self: Self, column: u32, row: u32) T {
+            return self.values[indexOf(column, row)];
+        }
+
+        pub fn set(self: *Self, column: u32, row: u32, value: T) void {
+            self.values[indexOf(column, row)] = value;
+        }
+
+        pub fn setColumn(self: *Self, column: u32, values: [R]T) void {
+
+            inline for(0..R) |row| {
+                self.set( column, row, values[row]);
+            }
+        }
+
+        pub fn setRow(self: *Self, row: u32, values: [C]T) void {
+
+            inline for(0..C) |column| {
+                self.set( column, row, values[column]);
+            }
+        }
+
+        pub fn setDiagonal(self: *Self, values: [C]T) void {
+
+            comptime if(!isQuadratic()) @compileError("Matrix must be quadratic");
+
+            inline for(0..C) |diagonal| {
+                self.set( diagonal, diagonal, values[diagonal]);
+            }
+        }
+
+        pub fn isQuadratic() bool {
+            return C == R;
+        }
+
+        pub fn transpose(self: Self) Self {
+
+            comptime if(!isQuadratic()) @compileError("Matrix must be quadratic");
+
+            var new: Self = undefined;
+            inline for(0..R) |row| {
+                inline for(0..C) |column| {
+                    const value = self.get(column, row);
+                    new.set(row, column, value);
+                }
+            }
+            return new;
+        }
+
+        pub fn add(self: Self, other: Self) Self {
+
+            var new: Self = undefined;
+            inline for(&new.values, self.values, other.values) |*value, a, b| {
+                value.* = a + b;
+            }
+            return new;
+        }
+
+        pub fn substract(self: Self, other: Self) Self {
+            return self.add(other.times(-1));
+        }
+
+        pub fn times(self: Self, scalar: T) Self {
+
+            var new: Self = undefined;
+            inline for(&new.values, self.values) |*value, v| {
+                value.* = v * scalar;
+            }
+            return new;
+        }
+
+        pub fn multiply(self: Self, other: anytype) Matrix(T,@TypeOf(other).C, R) {
+
+            const Other = @TypeOf(other);
+            comptime if(Self.C != Other.R) @compileError("Columns of self must equal rows of other");
+
+            var new: Matrix(T, Other.C, R) = undefined;
+
+            inline for(0..R) |row| {
+                inline for(0..Other.C) |column| {
+                    var sum: T = 0;
+                    inline for(0..C) |i| {
+                        const a = self.get(i, row);
+                        const b = other.get(column, i);
+                        sum = sum + a * b;
+                    }
+                    new.set(column, row, sum);
+                }
+            }
+            return new;
+        }
+
+        pub fn format(self: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+
+            _ = fmt;
+
+            const ff = std.fmt.format_float;
+
+            const valueOptions = ff.FormatOptions {
+                .mode = .decimal,
+                .precision = options.precision
+            };
+
+            var buffer: [ff.min_buffer_size]u8 = undefined;
+
+            inline for(0..R) |row| {
+                _ = try writer.write("( ");
+
+                inline for(0..C-1) |column| {
+                    const value = self.get(column, row);
+                    const slice = try std.fmt.formatFloat(&buffer, value, valueOptions);
+                    _ = try writer.write(slice);
+                    _ = try writer.write(", ");
+                }
+
+                const value = self.get(C-1, row);
+                const slice = try std.fmt.formatFloat(&buffer, value, valueOptions);
+                _ = try writer.write(slice);
+                _ = try writer.write(" )\n");
+            }
+        }
+    };
+}
+
+const Mat2 = Matrix(f32, 2, 2);
+const testing = std.testing;
+const tolerance = math.floatEps(f32);
+
+test "add" {
+    
+    const a = Mat2.identity();
+    const b = Mat2.all(1);
+    const c = a.add(b);
+    
+    try testing.expectApproxEqAbs(2, c.get(0, 0), tolerance);
+    try testing.expectApproxEqAbs(1, c.get(1, 0), tolerance);
+    try testing.expectApproxEqAbs(1, c.get(0, 1), tolerance);
+    try testing.expectApproxEqAbs(2, c.get(1, 1), tolerance);
+}
+
+test "subtract" {
+
+    const a = Mat2.identity();
+    const b = Mat2.all(1);
+    const c = a.substract(b);
+
+    try testing.expectApproxEqAbs(0, c.get(0, 0), tolerance);
+    try testing.expectApproxEqAbs(-1, c.get(1, 0), tolerance);
+    try testing.expectApproxEqAbs(-1, c.get(0, 1), tolerance);
+    try testing.expectApproxEqAbs(0, c.get(1, 1), tolerance);
+}
+
+test "multiply quadratic" {
+
+    const a = Mat2.of(.{ 1, 2, 3, 4 });
+    const b = Mat2.all(1);
+    const c = a.multiply(b);
+
+    try testing.expectApproxEqAbs(4, c.get(0, 0), tolerance);
+    try testing.expectApproxEqAbs(4, c.get(1, 0), tolerance);
+    try testing.expectApproxEqAbs(6, c.get(0, 1), tolerance);
+    try testing.expectApproxEqAbs(6, c.get(1, 1), tolerance);
+}
+
+test "multiply different shapes" {
+
+    const Mat4x2 = Matrix(f32, 4, 2);
+    const Matx2x4 = Matrix(f32, 2, 4);
+
+    const a = Mat4x2.of(.{ 1, 2, 3, 4, 5, 6, 7, 8 });
+    const b = Matx2x4.all(1);
+    const c = a.multiply(b);
+
+    try testing.expectEqual(2, @TypeOf(c).C);
+    try testing.expectEqual(2, @TypeOf(c).R);
+
+    try testing.expectApproxEqAbs(16, c.get(0, 0), tolerance);
+    try testing.expectApproxEqAbs(16, c.get(1, 0), tolerance);
+    try testing.expectApproxEqAbs(20, c.get(0, 1), tolerance);
+    try testing.expectApproxEqAbs(20, c.get(1, 1), tolerance);
+}
--- src/algebra/module.zig
+++ src/algebra/module.zig
@@ -1,2 +1,3 @@
 pub const Vector2 = @import("vector2.zig").Vector2;
 pub const Vector3 = @import("vector3.zig").Vector3;
+pub const Matrix = @import("matrix.zig").Matrix;
