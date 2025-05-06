e79394d557402934e7f5b20276a0cce92b46499d
Joel Lacour <silverclaw@noreply.codeberg.org>
2025-05-06 20:14:02 +0200

    tile registry  basics

--- src/world/Tile.zig
+++ src/world/Tile.zig
@@ -2,4 +2,25 @@ const coordinate = @import("coordinate");
 
 pub const Position = coordinate.Position(i32);
 
-pub const air = @This() {};
+pub const Index = u16;
+
+pub const Orientation = enum(u4) {
+    full,
+    half_north,
+    half_north_east,
+    half_south_east
+};
+
+const State = u12;
+
+/// A slot is what is saved directly in a Chunk grid.
+/// Additional data has to be resolved through the tile registry or from a linked TileEntity
+const Slot = struct {
+    index: Index,
+    orientation: Orientation,
+    state: State
+};
+
+
+pub const air: Index = 0;
+
