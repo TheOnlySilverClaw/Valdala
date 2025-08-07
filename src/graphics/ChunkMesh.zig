ef487711d4886885ae7f6b1e8ed78f0455f7d9eb
Joel Lacour <lacour.joel@web.de>
2025-08-07 20:42:37 +0200

    back to full tiles for now

--- src/graphics/ChunkMesh.zig
+++ src/graphics/ChunkMesh.zig
@@ -36,7 +36,7 @@ const grid = coordinate.Grid(i64, f32).of(hex);
 vertex_buffer: *webgpu.Buffer,
 index_buffer: *webgpu.Buffer,
 
-pub fn generate(camera: *const @import("scene").Camera, chunk: *const Chunk, device: *webgpu.Device) Self {
+pub fn generate(chunk: *const Chunk, device: *webgpu.Device) Self {
 
     const vertex_buffer_descriptor = webgpu.BufferDescriptor {
         .size = Chunk.Layout.volume * 6 * @sizeOf(Vertex),
@@ -70,8 +70,8 @@ pub fn generate(camera: *const @import("scene").Camera, chunk: *const Chunk, dev
         .height = 0
     });
 
-    for (32..33) |north| {
-        for (32..33) |south_east| {
+    for (0..Chunk.Layout.width) |north| {
+        for (0..Chunk.Layout.width) |south_east| {
             // TODO height
             const tile_offset = Chunk.TileOffset {
                 .north = @intCast(north),
@@ -89,14 +89,6 @@ pub fn generate(camera: *const @import("scene").Camera, chunk: *const Chunk, dev
             const tile = chunk.getTile(tile_offset);
             const center = grid.getCenter(tile_position).subtract(chunk_center);
             const vertices = generateTileVertices(tile, center, odd);
-            
-            const camera_matrix = camera.toMatrix();
-            for(vertices, 0..) |v, i| {
-
-                const vm = algebra.Matrix(f32, 1, 4).of(.{ v.position.x, v.position.y, v.position.z, 1 });
-                const p = camera_matrix.multiply(vm);
-                log.debug("v{} {d:.5} {d:.5} {d:.5}", .{ i, p.values[0], p.values[1], p.values[2] });
-            }
             var indices: [base_indices.len]u16 = undefined;
             for(base_indices, 0..) |base_index, index_number| {
                 indices[index_number] = @intCast(tile_index * vertices.len + base_index);
@@ -136,8 +128,6 @@ pub fn generateTileVertices(tile: Tile, center: Vector, odd: bool) [6]Vertex {
     const pos_se_top = Vertex.Position { .x = center.x + half_side, .y = center.y - hex.inradius, .z = hex.height };
     const pos_sw_top = Vertex.Position { .x = center.x - half_side, .y = center.y - hex.inradius, .z = hex.height };
     const pos_w_top = Vertex.Position { .x = center.x - hex.circumradius, .y = center.y, .z = hex.height };
-    const pos_w_center_top = Vertex.Position { .x = center.x - half_side, .y = center.y, .z = hex.height };
-    const pos_e_center_top = Vertex.Position { .x = center.x + half_side, .y = center.y, .z = hex.height };
 
     const uv_off: f16 = if(odd) 0.0 else 1.0;
     const uv_top_left = Vertex.UV { .u = uv_off, .v = uv_off };
@@ -145,8 +135,6 @@ pub fn generateTileVertices(tile: Tile, center: Vector, odd: bool) [6]Vertex {
     const uv_bottom_left = Vertex.UV { .u = uv_off, .v = 2.0 + uv_off };
     const uv_bottom_right = Vertex.UV { .u = 2.0 + uv_off, .v = 2.0 + uv_off };
     const uv_center = Vertex.UV { .u = 1.0 + uv_off, .v = 1.0 + uv_off };
-    const uv_center_left = Vertex.UV { .u = uv_off, .v = 1.0 + uv_off };
-    const uv_center_right = Vertex.UV { .u = 2.0 + uv_off, .v = 1.0 + uv_off };
 
     const vert_nw_top = Vertex { .position = pos_nw_top, .uv = uv_top_left, .texture = texture_top };
     const vert_ne_top = Vertex { .position = pos_ne_top, .uv = uv_top_right, .texture = texture_top };
@@ -154,18 +142,14 @@ pub fn generateTileVertices(tile: Tile, center: Vector, odd: bool) [6]Vertex {
     const vert_se_top = Vertex { .position = pos_se_top, .uv = uv_bottom_right, .texture = texture_top };
     const vert_sw_top = Vertex { .position = pos_sw_top, .uv = uv_bottom_left, .texture = texture_top };
     const vert_w_top = Vertex { .position = pos_w_top, .uv = uv_center, .texture = texture_top };
-    const vert_w_center_top = Vertex { .position = pos_w_center_top, .uv = uv_center_left, .texture = texture_top };
-    const vert_e_center_top = Vertex { .position = pos_e_center_top, .uv = uv_center_right, .texture = texture_top };
 
-    _ = vert_se_top;
-    _ = vert_sw_top;
 
     const vertices = [_]Vertex {
         vert_nw_top,
         vert_ne_top,
         vert_e_top,
-        vert_e_center_top,
-        vert_w_center_top,
+        vert_se_top,
+        vert_sw_top,
         vert_w_top
     };
     return vertices;
