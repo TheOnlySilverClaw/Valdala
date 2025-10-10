29ede11609c9bc1f1f53d98b8ecc5a22d07fe992
Joel Lacour <lacour.joel@web.de>
2025-10-10 18:02:43 +0200

    update root modules

--- build.zig
+++ build.zig
@@ -10,12 +10,28 @@ pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const main_module = b.createModule(.{
+        .optimize = optimize,
+        .target = target,
+        .root_source_file = b.path("src/main.zig")
+    });
+
+    const test_module = b.createModule(.{
+        .optimize = optimize,
+        .target = target,
+        .root_source_file = b.path("src/test.zig")
+    });
+
+    organizeModules(b, main_module, test_module, target, optimize);
+
     const exe = b.addExecutable(.{
         .name = "Valdala",
-        .root_source_file = b.path("src/main.zig"),
+        .root_module = main_module,
         .target = target,
         .optimize = optimize,
     });
+
+    linkLibraries(b, exe, target, optimize);
 
     b.installArtifact(exe);
 
@@ -29,7 +45,7 @@ pub fn build(b: *Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/test.zig"),
+        .root_module = test_module,
         .target = target,
         .optimize = optimize,
     });
@@ -37,10 +53,6 @@ pub fn build(b: *Build) void {
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
-
-
-    linkLibraries(b, exe, target, optimize);
-    organizeModules(b, exe.root_module, exe_unit_tests.root_module, target, optimize);
 }
 
 
