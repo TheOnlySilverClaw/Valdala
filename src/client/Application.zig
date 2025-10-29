7f81457e288ac53dd64f5fe9686f6cf3a2393709
hypercube <0hypercube@gmail.com>
2025-10-29 17:58:57 +0000

    Fix snake case (sorry)

--- src/client/Application.zig
+++ src/client/Application.zig
@@ -133,10 +133,10 @@ pub fn launch(self: *Self) !void {
         const input = self.controller.poll();
         if(input.window.close) break;
 
-        const cameraSpeed = 10; // in units per second
-        const deltaSeconds = @as(f32, @floatFromInt(delta)) / @as(f32, @floatFromInt(std.time.ns_per_s));
+        const camera_speed = 10; // in units per second
+        const delta_seconds = @as(f32, @floatFromInt(delta)) / @as(f32, @floatFromInt(std.time.ns_per_s));
 
-        const movementInWorldSpace = input.movement.rotation.project(input.movement.direction.times(deltaSeconds * cameraSpeed));
+        const movementInWorldSpace = input.movement.rotation.project(input.movement.direction.times(delta_seconds * camera_speed));
         player.transform.position = player.transform.position.add(movementInWorldSpace);
 
         player.transform.rotation = .aroundAxis(.of(-1,0,0), std.math.pi);
