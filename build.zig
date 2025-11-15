d8fc91e271e2fdff1e0bf86cf073dc8d3a5a1ae3
Joel Lacour <lacour.joel@web.de>
2025-11-15 16:09:22 +0100

    add umka dependency

--- build.zig
+++ build.zig
@@ -66,6 +66,7 @@ fn organizeModules(b: *std.Build, root: *Build.Module, test_root: *Build.Module,
     const yaml = b.dependency("yaml", .{}).module("yaml");
     const glfw = b.dependency("glfw", .{}).module("glfw");
     const webgpu = b.dependency("webgpu", .{}).module("webgpu");
+    const umka = b.dependency("umka", .{}).module("wrapper");
 
     // TODO move to separate repository?
     const glfw_webgpu = b.addModule("glfw-webgpu", .{
@@ -172,6 +173,7 @@ fn organizeModules(b: *std.Build, root: *Build.Module, test_root: *Build.Module,
     module.addImport("zigimg", zigimg);
     module.addImport("yaml", yaml);
     module.addImport("graphics", graphics);
+    module.addImport("umka", umka);
 
     gui.addImport("glfw", glfw);
     gui.addImport("webgpu", webgpu);
