d26b5ebe1f1a74a1f7f58f759d9bed03aebd8a8b
Joel Lacour <lacour.joel@web.de>
2025-11-23 10:11:01 +0100

    load diffrent example model

--- src/client/Application.zig
+++ src/client/Application.zig
@@ -134,10 +134,9 @@ pub fn launch(self: *Self) !void {
     const arena_allocator = arena.allocator();
 
     // TODO figure out where to put this
-    const cube_loaded_mesh = self.module_loader.entity_registry.entities.items[0].mesh;
+    const cube_loaded_mesh = self.module_loader.entity_registry.entities.items[1].mesh;
     
     var cube_entity_mesh = @import("scene").EntityMesh.init(surface.device, @ptrCast(cube_loaded_mesh.positions), cube_loaded_mesh.indices);
-    cube_entity_mesh.transform.scaleUniform(2.5);
     cube_entity_mesh.transform.moveZ(10.0);
 
     try scene.entities.append(allocator, cube_entity_mesh);
